26296dc6a9eacdfe7cef89ead07af19ba0b4a4fa
Brad <bw286@njit.edu>
2021-11-10 23:31:55 -0500

    changes

---
+++ public_html/other.php
@@ -0,0 +1,19 @@
+<?php include("login.php")?>
+<script src="pageutil.js"></script>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login_Page</title>
+  </head>
+
+  <body>
+    <div align="center">
+      <h2>Welcome to the main page</h2>
+      <input type="button" value="Go to back to login page" onclick="change_page('index.php')"/>
+    </div>	
+
+  </body>
+</html>
+
