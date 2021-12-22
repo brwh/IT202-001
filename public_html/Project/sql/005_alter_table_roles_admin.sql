62b18fc3ee1457668f4e2bc5d3243b06af212501
brwh <90637306+brwh@users.noreply.github.com>
2021-12-21 23:02:56 -0500

    Add files via upload

---
+++ public_html/Project/sql/005_alter_table_roles_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO Roles(id, name, is_active) VALUES(1, 'Admin', 1);
+INSERT INTO Users(account_number, user_id, balance, account_type,frozen,closed) VALUES (000000000000, 3, 0, 'world', 0, 0);
---
+++ public_html/Project/sql/008_create_world.sql
@@ -0,0 +1,1 @@
+INSERT INTO Users(account_number, user_id, balance, account_type,frozen,closed) VALUES (000000000000, 3, 0, 'world', 0, 0);
